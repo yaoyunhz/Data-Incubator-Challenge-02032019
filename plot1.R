b16de3afe920c63ae411b3329b05f173be83bb8d
Yun Yao <yaoyunhz@gmail.com>
2019-02-03 21:20:25 -0700

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-#plot1
+#plot2
 library(lubridate)
 
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
@@ -6,14 +6,13 @@ data$Date <- dmy(data$Date)
 data <- data[(data$Date == "2007-02-01" | data$Date == "2007-02-02"), ]
 data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
 
-dev.new(width = 480, height = 480)
-hist(data$Global_active_power,
-     breaks = 12,
-     ylim = c(0, 1200),
-     col = "red",
-     main = "Global Active Power",
-     xlab = "Glabal Active Power (kilowatts)")
-axis(1, at = seq(0, 6, by = 2))
+data$day <- paste(data$Date, data$Time)
+data$day <- ymd_hms(data$day)
 
-dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.new(width = 480, height = 480)
+with(data, 
+     plot(day, Global_active_power, 
+          type = "l", ylab = "Global Active Power (kilowatts)"))
+
+dev.copy(png, file = "plot2.png", width = 480, height = 480)
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,43 +1,20 @@
-#plot2
+#plot3
 library(lubridate)
-data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 data$Date <- dmy(data$Date)
 data <- data[(data$Date == "2007-02-01" | data$Date == "2007-02-02"), ]
-data$datetime <- paste(data$Date, data$Time)
-data$datetime <- ymd_hms(data$datetime)
+data$day <- paste(data$Date, data$Time)
+data$day <- ymd_hms(data$day)
 
-data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
-data$Voltage <- as.numeric(as.character(data$Voltage))
-data$Global_reactive_power <- as.numeric(as.character(data$Global_reactive_power))
-
-#four plots
 dev.new(width = 480, height = 480)
-par(mfrow = c(2, 2), mar = c(5, 5, 2, 1))
-
-##plot1
-with(data,
-     plot(datetime, Global_active_power,
-          type = "l", ylab = "Global Active Power"))
-
-##plot2
-with(data,
-     plot(datetime, Voltage,
-          type = "l", ylab = "Voltage"))
-
-##plot3
-with(data, plot(datetime, Sub_metering_1, type = "n",
+with(data, plot(day, Sub_metering_1, type = "n", 
                 ylab = "Engergy sub metering", xlab = ""))
-with(data, lines(datetime, Sub_metering_1, col = "black"))
-with(data, lines(datetime, Sub_metering_2, col = "red"))
-with(data, lines(datetime, Sub_metering_3, col = "blue"))
-legend("topright", lty = 1, cex = 0.8, col = c("black", "red", "blue"),
+with(data, lines(day, Sub_metering_1, col = "black"))
+with(data, lines(day, Sub_metering_2, col = "red"))
+with(data, lines(day, Sub_metering_3, col = "blue"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), cex = 0.7,
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-##plot4
-with(data,
-     plot(datetime, Global_reactive_power,
-          type = "l", ylim = c(0, 0.5)))
-
-dev.copy(png, file = "plot2.png", width = 480, height = 480)
+dev.copy(png, file = "plot3.png", width = 480, height = 480)
 dev.off()
